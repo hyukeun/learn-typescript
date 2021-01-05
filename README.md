0aff0cb3793a3ccc4266032827d1302dfa69860a
hyukeun <65886697+hyukeun@users.noreply.github.com>
2021-01-06 00:02:37 +0900

    Typescript : basic

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # learn-typescript
+
+## Typescript를 공부하며 기록 및 저장하는 공간입니다.
---
+++ utill.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function add(a, b) {
+    return a + b;
+}
+exports.default = add;
+// export 한 것을 hello.ts로 import해서 가져다 사용할 수 있다. 
